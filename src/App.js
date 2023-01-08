6f9beec3c4bfde3afa54a4db22adaff1e293363c
wspawned <wspawned@gmail.com>
2023-01-09 00:38:16 +0300

    look later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
 
+import Routers from './routes'
+
+import {setAuthToken} from './helpers/setAuthToken'
+
 function App() {
+
+  //check jwt token
+  const token = localStorage.getItem("token");
+  if (token) {
+      setAuthToken(token);
+  }
+
   return (
     <div className="App">
-      App
+      <Routers/>
     </div>
   );
 }
